f213160281dd295deef2934a610501ebd344aa44
Luis Egusquiza <egusquiza.l@pucp.edu.pe>
2024-04-05 16:30:47 -0500

    new data

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -13,101 +13,122 @@ def load_data(url):
     csv_raw = StringIO(response.text)
     return pd.read_csv(csv_raw)
 
-# Cargar los datos de GeoJSON para el mapa de las regiones de Chile
+# Cargar datos (raw-github)
 geojson_url = 'https://raw.githubusercontent.com/fcortes/Chile-GeoJSON/master/Regional.geojson'
 chile_geojson = requests.get(geojson_url).json()
-
-# Cargar datos para el mapa coroplético
 region_summary_url = 'https://raw.githubusercontent.com/LuisEgus/Streamlit---Dashboard/main/data%20CHILE/dta/region_summary.csv'
-df_region = load_data(region_summary_url)
-
-# Cargar datos para el vector de calor
 sector_summary_url = 'https://raw.githubusercontent.com/LuisEgus/Streamlit---Dashboard/main/data%20CHILE/dta/sector_summary.csv'
+industry_summary_url = 'https://raw.githubusercontent.com/LuisEgus/Streamlit---Dashboard/main/data%20CHILE/dta/industry_summary.csv'
+buyer_summary_url = 'https://raw.githubusercontent.com/LuisEgus/Streamlit---Dashboard/main/data%20CHILE/dta/buyer_summary.csv'
+
+
+# Cargar los dataset como dataframe
+df_region = load_data(region_summary_url)
 df_sector = load_data(sector_summary_url)
-
-# Cargar datos para industria
-df_industry = load_data('https://raw.githubusercontent.com/LuisEgus/Streamlit---Dashboard/main/data%20CHILE/dta/industry_summary.csv')
-
+df_industry = load_data(industry_summary_url)
+df_buyer = load_data(buyer_summary_url)
 
 # Sidebar - Selección de tipo de test
 with st.sidebar:
     st.title('🌎 Chile Data Dashboard')
     test_type = st.selectbox('Select Test Type', df_region['test_type'].unique())
+    sector = st.sidebar.selectbox('Select Sector', df_buyer['sector'].unique())
     color_theme = st.selectbox('Select Color Theme', ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis'])
 
-# Filtrar los datos basados en el tipo de test seleccionado
-df_filtered = df_region[df_region['test_type'] == test_type]
-df_sector_filtered = df_sector[df_sector['test_type'] == test_type]
-df_industry_filtered = df_industry[df_industry['test_type'] == test_type]
-
-# Asegurarse de que no haya valores NaN que puedan afectar la visualización
-df_filtered['beta_robust'].fillna(0, inplace=True)
-df_filtered['p_value'].fillna(1, inplace=True)  # Puede elegir otro valor por defecto que tenga sentido en su contexto
-df_filtered['num_observ'].fillna(0, inplace=True)
+# Filtrar datos
+df_filtered = df_region[df_region['test_type'] == test_type].fillna(0)
+df_sector_filtered = df_sector[df_sector['test_type'] == test_type].fillna(0)
+df_industry_filtered = df_industry[df_industry['test_type'] == test_type].fillna(0)
+df_buyer_filtered = df_buyer[(df_buyer['test_type'] == test_type) & (df_buyer['sector'] == sector)]
 
 # Crear un mapa coroplético centrado en Chile
-fig_chile = px.choropleth(df_filtered,
-                          geojson=chile_geojson,
-                          locations='codregion',
-                          color='beta_robust',
-                          featureidkey="properties.codregion",
-                          hover_data={'beta_robust': True, 'p_value': True, 'num_observ': True}
-                         )
+def create_choropleth(df):
+    
+    fig = px.choropleth(df,
+                        geojson=chile_geojson,
+                        locations='codregion',
+                        color='beta_robust',
+                        color_continuous_scale=color_theme,
+                        featureidkey="properties.codregion",
+                        hover_data={'beta_robust': True, 'p_value': True, 'num_observ': True}
+                       )
+    fig.update_geos(
+        fitbounds="locations",
+        visible=True,
+        showcountries=True,
+        showcoastlines=False,
+        showland=False,
+        showocean=False,
+        center={"lat": -35.6751, "lon": -71.543}
+        # Elimina la línea de 'projection': {"type": "mercator"}
+    )
+    fig.update_layout(
+        margin={"r":0, "t":0, "l":0, "b":0},  # Reduce los márgenes si es necesario
+        height=800  # Ajusta la altura para aumentar la longitud del mapa
+    )
+    fig.update_traces(marker_line_color='black', marker_line_width=0.5)  # Puedes ajustar el grosor de las líneas aquí
+    return fig
 
-fig_chile.update_geos(
-    fitbounds="locations",
-    visible=True,
-    showcountries=False,
-    showcoastlines=False,
-    showland=False,
-    showocean=False,
-    center={"lat": -35.6751, "lon": -71.543},
-    projection_scale=5  # Ajustar para mejorar el ajuste al área geográfica de Chile
-)
+fig_chile1 = create_choropleth(df_filtered)
+fig_chile2 = create_choropleth(df_filtered)
 
-fig_chile.update_layout(
-    margin={"r":0, "t":0, "l":0, "b":0},
-    height=600
-)
 
-fig_chile.update_traces(marker_line_color='black', marker_line_width=1)
+fig_chile1 = create_choropleth(df_filtered)
+fig_chile2 = create_choropleth(df_filtered)
 
 # Crear un gráfico de vector de calor para los sectores
 fig_heatmap = px.density_heatmap(df_sector_filtered,
-                                 x='sector',
-                                 y='test_type',
+                                 x='test_type',
+                                 y='sector',
                                  z='beta_robust',
-                                 hover_data=['beta_robust', 'p_value', 'num_observ'],
+                                 color_continuous_scale=color_theme,
                                  labels={'beta_robust':'Beta Robust', 'p_value':'P-Value', 'num_observ':'Number of Observations'},
-                                 title='Heatmap de Sectores')
-
+                                 title='Vector of sectors'
+                                 )
 
 # Creación de la matriz de calor para la industria
-fig_heatmap_industry = px.density_heatmap(
-    df_industry_filtered,
-    x='rubro3', 
-    y='zone', 
-    z='beta_robust', 
-    color_continuous_scale=color_theme, 
-    title='Heatmap de Industrias',
-    hover_data={'beta_robust': ':.2f', 'p_value': ':.2f', 'num_observ': ':.0f'}
+fig_heatmap_industry = px.density_heatmap(df_industry_filtered,
+                                          x='rubro3', 
+                                          y='zone', 
+                                          z='beta_robust', 
+                                          color_continuous_scale=color_theme,
+                                          title='Matrix of macrozones'
+                                         )
+fig_heatmap_industry.update_traces(hovertemplate='<b>%{x}</b><br>%{y}<br>Beta Robust: %{z:.2f}<br>P-value: %{customdata[0]:.2f}<br>Num. Observ: %{customdata[1]:.0f}',
+                                   customdata=df_industry_filtered[['p_value', 'num_observ']].values)
+fig_heatmap_industry.update_layout(height=600, margin={"r":0, "t":50, "l":0, "b":100})
+
+
+# Crear el gráfico de barras para Buyer Summary filtrado
+fig_bar = px.bar(
+    df_buyer_filtered,
+    x='rutunidadcompra',  # Asumiendo que 'rutunidadcompra' es la columna correcta para los compradores
+    y='beta_robust',
+    title='Bar Chart of Buyers',
+    labels={'rutunidadcompra': 'Buyer', 'beta_robust': 'Beta Robust'},
+    hover_data=['p_value', 'num_observ']  # Agregar más datos al hover
 )
 
-# Agregar etiquetas de datos con 'hover_data'
-fig_heatmap_industry.update_traces(
-    hovertemplate='<b>%{x}</b><br>%{y}<br>Beta Robust: %{z:.2f}<br>P-value: %{customdata[0]:.2f}<br>Num. Observ: %{customdata[1]:.0f}',
-    customdata=df_industry_filtered[['p_value', 'num_observ']].values
+# Ajustar los estilos del gráfico de barras
+fig_bar.update_layout(
+    margin={"r":0, "t":50, "l":0, "b":0},
+    height=400
 )
 
-# Actualizar estilos del gráfico de matriz de calor
-fig_heatmap_industry.update_layout(
-    margin={"r":0, "t":50, "l":0, "b":100},
-    height=600,
-    plot_bgcolor='rgba(0,0,0,0)',
-    paper_bgcolor='rgba(0,0,0,0)'
-)
 
-# Mostrar gráficos en el dashboard
-st.plotly_chart(fig_chile, use_container_width=True)
-st.plotly_chart(fig_heatmap, use_container_width=True)
+# Distribución de gráficos en el dashboard
+col1, col2, col3 = st.columns([1, 1, 1])  # Ajusta las proporciones si es necesario
+
+with col1:
+    st.plotly_chart(fig_chile1, use_container_width=True)
+
+with col2:
+    st.plotly_chart(fig_chile2, use_container_width=True)
+
+with col3:
+    st.plotly_chart(fig_heatmap, use_container_width=True)
+
 st.plotly_chart(fig_heatmap_industry, use_container_width=True)
+
+# Mostrar el gráfico de barras debajo de los otros gráficos
+st.plotly_chart(fig_bar, use_container_width=True)
